bfd5d9547c9bb31120c05330527351ebdc14aadb
zlatan-dp <99171484+zlatan-dp@users.noreply.github.com>
2022-06-14 11:44:56 +0000

    Deploying to gh-pages from @ zlatan-dp/iceten-project@8baf403a298a619f0f434b102cdde9804cd9d4b4 🚀

--- index.3f03a74c.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector(".menu-order__btn"),closeModalBtn:document.querySelector(".modal__close-btn"),modal:document.querySelector(".backdrop-modal")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.3f03a74c.js.map
---
+++ index.8bda0097.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector(".buy-open"),closeModalBtn:document.querySelector(".modal__close-btn"),modal:document.querySelector(".backdrop-modal")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.8bda0097.js.map
