233d71e2688900f9cb326482710bca1323cfaee4
zlatan-dp <99171484+zlatan-dp@users.noreply.github.com>
2022-06-15 15:16:13 +0000

    Deploying to gh-pages from @ zlatan-dp/iceten-project@b8b12299c5ea6d343e31c1a7feb39cbb9f0c8032 🚀

---
+++ index.ec432b51.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalgeoBtn:document.querySelector(".btn-location"),closeModalgeoBtn:document.querySelector(".geomodal-close-btn"),modalgeo:document.querySelector(".geo-backdrop")};function o(){e.modalgeo.classList.toggle("is-hidden")}e.openModalgeoBtn.addEventListener("click",o),e.closeModalgeoBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.ec432b51.js.map
